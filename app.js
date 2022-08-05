7692eeaa2a52a9e907d8c19c762f912d486d04fe
kristafrancis721 <kristafrancis08@yahoo.com>
2022-08-05 13:28:24 -0400

    add feature questions required questions

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: nameInput => {
+          if (nameInput) { 
+            return true;
+          } else {
+            console.log('Please enter your Github Username');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
@@ -37,12 +53,28 @@ if (!portfolioData.projects) {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your project name!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your project description!');
+            return false;
+          }
+        }
       },
       {
         type: 'checkbox',
@@ -53,7 +85,15 @@ if (!portfolioData.projects) {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your Github link to your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'confirm',
